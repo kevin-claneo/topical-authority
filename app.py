eef62abf7c88d28d9adf2d21b7b4e6e337607f07
kevin-claneo <108477383+kevin-claneo@users.noreply.github.com>
2024-05-02 17:28:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import concurrent.futures
 import time
 
 # Related third-party imports
-import streamlit as st
+from pyvis import pyvis.network as net
 from streamlit_elements import Elements
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
